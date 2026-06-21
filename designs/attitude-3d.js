b06f551d1d7f56aa24f8f10799600d1f29585062
Huang Zhaoming <116700814+dongge0210@users.noreply.github.com>
2026-06-21 20:53:46 +0800

    fix: invert pitch direction in 3D model rotation

--- designs/attitude-3d.js
+++ designs/attitude-3d.js
@@ -188,7 +188,7 @@ export function update(data) {
   // Pitch = rotate around device-X (world-X), Roll = rotate around device-Y (world-Z)
   macbook.rotation.order = 'XZY';
   macbook.rotation.x = -roll * (Math.PI / 180);
-  macbook.rotation.z = -pitch * (Math.PI / 180);
+  macbook.rotation.z = pitch * (Math.PI / 180);
   // Lid angle: 0=closed, 113=normal open. Hinge at 0° = vertical.
   if (lidAngle != null)
     hingePivot.rotation.x = -(lidAngle - 90) * (Math.PI / 180);
